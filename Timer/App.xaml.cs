d0391e01f2123ff4b14bb87d2dc47246c63b7d59
willcod <william.wow@icloud.com>
2022-03-28 22:42:53 +0800

    Add project files.

---
+++ Timer/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+using Timer.Views;
+
+namespace Timer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+    }
+}
---
+++ Timer/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Threading;
+using Prism.Mvvm;
+
+namespace Timer.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private string _content;
+
+        private DateTime _startTime;
+
+        public MainViewModel()
+        {
+            _startTime = DateTime.Now;
+
+            var timer = new DispatcherTimer(DispatcherPriority.Normal);
+            timer.Interval = TimeSpan.FromMilliseconds(5);
+            timer.Tick += (sender, args) =>
+            {
+                Content = (DateTime.Now - _startTime).ToString(@"hh\:mm\:ss\.fff");
+            };
+            timer.Start();
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { SetProperty(ref _content, value); }
+        }
+    }
+}
